1ca5c0a2e1f3e5888946492deed710b2b9d0242e
Eric Upchurch <Eric.upchurch.eu@gmail.com>
2022-05-07 16:04:48 -0600

    Suppressed values within function

--- Interior_Point_Fun.m
+++ Interior_Point_Fun.m
@@ -43,23 +43,23 @@ lambda_minus=tand(theta_minus-alpha_minus);
 
 %%
 %Compatiblity equation coefficients 
-A_plus = u_plus.^2-a_plus.^2
-A_minus = u_minus.^2-a_minus.^2
-B_plus= 2.*u_plus.*v_plus-(u_plus.^2-a_plus.^2).*lambda_plus
-B_minus=2.*u_minus.*v_minus-(u_minus.^2-a_minus.^2).*lambda_minus
-C_plus= omega.*a_plus.^2.*v_plus./y_plus
-C_minus= omega.*a_minus.^2.*v_minus./y_minus
-D_plus  = A_plus.*u_plus+B_plus.*v_plus-C_plus.*x_plus        %RHS of +characteristic compatability eq after rearrangement
-D_minus = A_minus.*u_minus+B_minus.*v_minus-C_minus.*x_minus  %RHS of -characteristic compatability eq after rearrangement
+A_plus = u_plus.^2-a_plus.^2;
+A_minus = u_minus.^2-a_minus.^2;
+B_plus= 2.*u_plus.*v_plus-(u_plus.^2-a_plus.^2).*lambda_plus;
+B_minus=2.*u_minus.*v_minus-(u_minus.^2-a_minus.^2).*lambda_minus;
+C_plus= omega.*a_plus.^2.*v_plus./y_plus;
+C_minus= omega.*a_minus.^2.*v_minus./y_minus;
+D_plus  = A_plus.*u_plus+B_plus.*v_plus-C_plus.*x_plus;        %RHS of +characteristic compatability eq after rearrangement
+D_minus = A_minus.*u_minus+B_minus.*v_minus-C_minus.*x_minus;  %RHS of -characteristic compatability eq after rearrangement
 
-F=[-lambda_plus 1 0 0 ; -lambda_minus 1 0 0 ; -C_plus 0 A_plus B_plus ; -C_minus 0 A_minus B_minus ] %matrix of coefficients for characteristic and compatibility finite difference equations
-G=[y_plus-lambda_plus*x_plus; y_minus-lambda_minus*x_minus ; D_plus ; D_minus] %column vector containing constants each of the finite difference equations are equal to
-F_in=inv(F)
-x_y_u_v=F_in*G
+F=[-lambda_plus 1 0 0 ; -lambda_minus 1 0 0 ; -C_plus 0 A_plus B_plus ; -C_minus 0 A_minus B_minus ]; %matrix of coefficients for characteristic and compatibility finite difference equations
+G=[y_plus-lambda_plus*x_plus; y_minus-lambda_minus*x_minus ; D_plus ; D_minus]                      :  %column vector containing constants each of the finite difference equations are equal to
+F_in=inv(F);
+x_y_u_v=F_in*G;
 
-x=x_y_u_v(1)
-y=x_y_u_v(2)
-u= x_y_u_v(3)
-v= x_y_u_v(4)
+x=x_y_u_v(1);
+y=x_y_u_v(2);
+u= x_y_u_v(3);
+v= x_y_u_v(4);
 
 end
