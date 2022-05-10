a17e069330afe59dacc4c74471e91873fa2fc45c
Lgordon8 <42584485+Lgordon8@users.noreply.github.com>
2022-05-10 16:47:48 -0500

    Add files via upload

--- exit_point_fun.m
+++ exit_point_fun.m
@@ -1,4 +1,4 @@
-function [Exit_out] = exit_point_fun(plus_in, minus_in, Pressure, Temperature, R, gamma, omega)
+function [x0,y0,u0,v0] = exit_point_fun(plus_in, minus_in, Pressure, Temperature, R, gamma, omega)
 %This function calculates the compatibility equations of the Exit
 %   X_pos and x_in are the inputs form the characteristic lines with a
 %   posistive and negative slope respectifully
@@ -36,21 +36,22 @@ theta_min=atand(v_min/u_min);
 
 %Positive compatibility equations
 L_pos=tand(theta_pos+alpha_pos);
-A_pos=u_pos^2-a_pos^2;
-B_pos=2*u_pos*v_pos-(u_pos^2-a_pos^1)*L_pos;
-C_pos=omega*a_pos^2*v_pos/y_pos;
-D1_pos=1;
-D2_pos=-1;
-D3_pos=0.25;
-a=D3_pos;
-b=(D2_pos-L_pos);
-c=D1_pos+L_pos*x_pos-y_pos;
-x0=(-b+sqrt(b^2-4*a*c))/(2*a);
 
-y0=L_pos*(x0-x_pos)+y_pos;
-F=C_pos*x0+A_pos*u_pos+B_pos*v_pos-C_pos*x_pos;
-u0=F/(A_pos+B_pos*(D2_pos+2*D3_pos*x0));
-v0=u0*(D2_pos+2*D3_pos*x0);
 
-Exit_Out=[x0,y0,u0,v0];
+% A_pos=u_pos^2-a_pos^2;
+% B_pos=2*u_pos*v_pos-(u_pos^2-a_pos^1)*L_pos;
+% C_pos=omega*a_pos^2*v_pos/y_pos;
+% D1_pos=1;
+% D2_pos=-1;
+% D3_pos=0.25;
+% a=D3_pos;
+% b=(D2_pos-L_pos);
+% c=D1_pos+L_pos*x_pos-y_pos;
+% x0=(-b+sqrt(b^2-4*a*c))/(2*a);
+% 
+% y0=L_pos*(x0-x_pos)+y_pos;
+% F=C_pos*x0+A_pos*u_pos+B_pos*v_pos-C_pos*x_pos;
+% u0=F/(A_pos+B_pos*(D2_pos+2*D3_pos*x0));
+% v0=u0*(D2_pos+2*D3_pos*x0);
+
 end
