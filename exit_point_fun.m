8694d7647bbb90b6674a98814418fa3c54cb78ee
Lgordon8 <42584485+Lgordon8@users.noreply.github.com>
2022-05-11 11:58:12 -0500

    Add files via upload

--- exit_point_fun.m
+++ exit_point_fun.m
@@ -1,54 +1,60 @@
-function [x0,y0,u0,v0] = exit_point_fun(plus_in, minus_in, Pressure, Temperature, R, gamma, omega, Patm)
+function [x0,y0,u0,v0] = exit_point_fun(plus_in, minus_in, Pressure, Temperature, R, gamma, Patm)
 %This function calculates the compatibility equations of the Exit
 %   X_pos and x_in are the inputs form the characteristic lines with a
 %   posistive and negative slope respectifully
 
-x_pos=plus_in(1);
-y_pos=plus_in(2);
-u_pos=plus_in(3);
-v_pos=plus_in(4);
+x2=plus_in(1);%Positive input
+y2=plus_in(2);
+u2=plus_in(3);
+v2=plus_in(4);
 
-x_min=minus_in(1);
-y_min=minus_in(2);
-u_min=minus_in(3);
-v_min=minus_in(4);
+x3=minus_in(1);%Negative input
+y3=minus_in(2);
+u3=minus_in(3);
+v3=minus_in(4);
 
 %Setting the Temperatur and pressure and Total Speed of Sound
 T=Temperature;
 P=Pressure;
+Pt=Patm;
 atot=sqrt(gamma*R*T);
 
 %Positive characteritics line
-%V_pos=sqrt(((2*gamma*R*T)/(gamma-1)*(1-(P/Pt)^((gamma-1)/gamma))));
-V_pos=sqrt(u_pos^2+v_pos^2);
-a_pos=sqrt(atot^2-(gamma-1)*V_pos^2/2);
-M_pos=V_pos/a1;
-alpha_pos=asin(1/M_pos);
-theta_pos=atan(v_pos/u_pos);
+V4=sqrt(((2*gamma*R*T)/(gamma-1)*(1-(P/Pt)^((gamma-1)/gamma))));
+V2=sqrt(u2^2+v2^2);
+a2=sqrt(atot^2-(gamma-1)*V2^2/2);
+M2=V_pos/a2;
+alpha2=asin(1/M2);
+theta2=atan(v2/u2);
 
 %Negative Characteristic Line
 %V_min=sqrt(((2*gamma*R*T)/(gamma-1)*(1-(P/Pt)^((gamma-1)/gamma))));
-V_min=sqrt(u_min^2+v_min^2);
-a_min=sqrt(atot^2-(gamma-1)*V_min^2/2);
-M_min=V_min/a_min;
-alpha_min=asind(1/M_min);
-theta_min=atand(v_min/u_min);
+V3=sqrt(u3^2+v3^2);
+a3=sqrt(atot^2-(gamma-1)*V3^2/2);
+M3=V3/a3;
+alpha3=asind(1/M3);
+theta3=atand(v3/u3);
 
 %Positive compatibility equations
-L_pos=tand(theta_pos+alpha_pos);
+L_pos=tand(theta2+alpha2);
 Q_pos=u2^2-a2^2;
 R_pos=2*u2*v2-Q_pos*L_pos;
 S_pos=(a2^2*V2)/y3;
+
+%x and y along the streamline
+x4=(y3-L0*x3-y2+L_pos*x2)/L_pos;
+y4=y2-L_pos*x2+L_pos*x4;
+
 T_pos=S_pos*(x4-x2)+Q_pos*u2+R_pos*v2;
+
+%Stremanwise and transverse velocities
 u4=((Q_pos*T_pos)-(R_pos*V4^2*(Q_pos^2+R_pos^2)-T_pos^2)^(1/2))/(Q_pos^2+R_pos^2);
 v4=(V4^2-u^2)^(1/2);
-y4=y2-L_pos*x2+L_pos*x4;
-x4=((y3-y4)/L_0)-x3;
+
 y0=y4;
 x0=x4;
 u0=u4;
 v0=v4;
-
 
 % A_pos=u_pos^2-a_pos^2;
 % B_pos=2*u_pos*v_pos-(u_pos^2-a_pos^1)*L_pos;
